09ae32229ea3c4e4077e55f77f8cb4ee75ecee74
Kyle Coffey <me@phytolizer.dev>
2022-09-12 22:04:51 -0500

    linenoise

--- src/main.c
+++ src/main.c
@@ -1,5 +1,28 @@
+#include <linenoise.h>
 #include <println/println.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(void) {
-    printfln("Hello, world!");
+    linenoiseHistoryLoad("minsc.history");
+
+    while (true) {
+        char* line = linenoise(">> ");
+        if (line == NULL) {
+            break;
+        }
+
+        linenoiseHistoryAdd(line);
+
+        if (strcmp(line, "1 + 2 * 3") == 0) {
+            printfln("7");
+        } else {
+            printfln("ERROR: Invalid expression!");
+        }
+
+        free(line);
+    }
+
+    linenoiseHistorySave("minsc.history");
 }
