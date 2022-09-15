fdf9b2fcab7ecd61f9df59020a7731b1438b01d1
Kyle Coffey <me@phytolizer.dev>
2022-09-15 16:12:47 -0500

    no need to call strlen on static strings

--- src/syntax_kind.c
+++ src/syntax_kind.c
@@ -8,12 +8,19 @@ static const char* const SYNTAX_KIND_STRINGS[] = {
 #undef X
 };
 
+static const size_t SYNTAX_KIND_STRING_LENGTHS[] = {
+#define X(x) sizeof(#x) - 1,
+#include "syntax_kind_uppercamel.inc"
+#undef X
+};
+
 extern str syntax_kind_string(SyntaxKind kind) {
     switch (kind) {
 #define X(x) case SYNTAX_KIND_##x:
 #include "syntax_kind.inc"
 #undef X
-        return str_ref(SYNTAX_KIND_STRINGS[kind]);
+        return str_ref_chars(SYNTAX_KIND_STRINGS[kind],
+                             SYNTAX_KIND_STRING_LENGTHS[kind]);
         default:
             return str_lit("SYNTAX_KIND_UNKNOWN");
     }
