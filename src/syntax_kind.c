abf998a427163d1e5812981e8a793512165b7ca3
Kyle Coffey <me@phytolizer.dev>
2022-09-15 09:55:54 -0500

    use the new code for error messages

--- src/syntax_kind.c
+++ src/syntax_kind.c
@@ -1,14 +1,20 @@
 #include "syntax_kind.h"
 
+#include <str/str.h>
+
+static const char* const SYNTAX_KIND_STRINGS[] = {
+#define X(x) #x,
+#include "syntax_kind_uppercamel.inc"
+#undef X
+};
+
 extern str syntax_kind_string(SyntaxKind kind) {
     switch (kind) {
-#define X(x) \
-    case SYNTAX_KIND_##x: \
-        return str_lit(#x);
-#include "str/str.h"
+#define X(x) case SYNTAX_KIND_##x:
 #include "syntax_kind.inc"
-
 #undef X
+        return str_ref(SYNTAX_KIND_STRINGS[kind]);
+        default:
+            return str_lit("SYNTAX_KIND_UNKNOWN");
     }
-    return str_lit("SYNTAX_KIND_UNKNOWN");
 }
