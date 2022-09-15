e0f1e6450b0715b58174debdb93e049d96573463
Kyle Coffey <me@phytolizer.dev>
2022-09-15 09:58:43 -0500

    tweak VaryCase.cmake

--- cmake/VaryCase.cmake
+++ cmake/VaryCase.cmake
@@ -7,17 +7,17 @@ macro(vary_case FILEPATH OUTPATH CASE TARGET)
   )
   set(_outfilepath "${_outpath}/${_filestem}_${CASE}.inc")
   cmake_path(ABSOLUTE_PATH _filepath OUTPUT_VARIABLE _infilepath)
-  set(_targetname ${TARGET}_${_filestem}_${CASE})
+  set(_targetname generate_${TARGET}_${_filestem}_${CASE})
   add_custom_command(
     OUTPUT "${_outfilepath}"
     DEPENDS varycase::varycase "${_infilepath}"
     COMMAND varycase::varycase ${CASE} "${_infilepath}" -o "${_outfilepath}"
   )
-  add_custom_target(generate_${_targetname} DEPENDS "${_outfilepath}")
+  add_custom_target(${_targetname} DEPENDS "${_outfilepath}")
   if(TARGET ${TARGET})
     target_include_directories(
       ${TARGET} PRIVATE ${CMAKE_CURRENT_BINARY_DIR}/include
     )
-    add_dependencies(${TARGET} generate_${_targetname})
+    add_dependencies(${TARGET} ${_targetname})
   endif()
 endmacro()
